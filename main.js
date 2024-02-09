aa63cf2d2b3a0e8bfaa353e1ded3f565d7e14f0b
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2024-02-09 21:12:23 +0400

    Main

---
+++ main.js
@@ -0,0 +1,31 @@
+const createCard = () => {
+    const mainContainer = document.body.querySelector(".container");
+
+    const cardDiv = document.createElement("div");
+    cardDiv.id = "card";
+
+    const heading = document.createElement("h2");
+    heading.textContent = "Thom Yorke";
+
+    const image = document.createElement("img");
+    image.src = "./images/profile.jpg";
+    image.id = "profileImg";
+
+    const description = document.createElement("p");
+    description.textContent = "Radiohead's lead singer";
+    description.id = "description";
+
+    const anchor = document.createElement("a");
+    anchor.href = "#";
+    anchor.textContent = "Go to profile";
+    anchor.id = "hrefAnchor";
+
+    cardDiv.appendChild(heading);
+    cardDiv.appendChild(image);
+    cardDiv.appendChild(description);
+    cardDiv.appendChild(anchor);
+
+    mainContainer.appendChild(cardDiv);
+};
+
+createCard();
